6fd99c4524734d5b0f0bc7bd68fc5c8b1832a00f
huynhlv <huynhlevan1997@gmail.com>
2019-05-16 10:51:02 +0700

    Challenge 2

---
+++ Exam/Ruby/challenge_2.rb
@@ -0,0 +1,9 @@
+def LongestWord sen
+  arr = sen.split(" ").each {|x| x.gsub!(/[^a-zA-Z0-9]/, "")}
+  max = arr[0]
+  arr.each {|x| max = x if max.length < x.length}
+  max
+end
+
+sen = "fun&!! time"
+puts LongestWord(sen)
