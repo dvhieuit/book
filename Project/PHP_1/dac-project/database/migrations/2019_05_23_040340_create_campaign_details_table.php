fd7f9f999e63f16932156ca539c7729f9c62d86e
Thong <baibuvietnam@gmail.com>
2019-05-23 11:39:11 +0700

    create migration

--- Project/PHP_1/dac-project/database/migrations/2019_05_23_040340_create_campaign_details_table.php
+++ Project/PHP_1/dac-project/database/migrations/2019_05_23_040340_create_campaign_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCampaignDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('Users', function (Blueprint $table) {
+        Schema::create('CampaignDetails', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('fullname');
-            $table->integer('role_id');
-            $table->string('phone_number');
-            $table->rememberToken();
+            $table->integer('campaign_id');
+            $table->integer('views');
+            $table->integer('clicks');
+            $table->datetime('datetime');
         });
     }
 
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Users');
+        Schema::dropIfExists('CampaignDetails');
     }
 }
