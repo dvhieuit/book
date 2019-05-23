9398621c481a63e4e8c4dc8c76d3e7a55d1e4ae0
LE BA HUNG <hung.lb@dactech.vn>
2019-05-23 16:11:15 +0700

    -  [Java][Spring web]: add cross to debug in local side

--- Project/Java/Le_Ba_Hung/Exam/Spring_boot_web/api/src/main/java/com/dac/demo/controller/AdminController.java
+++ Project/Java/Le_Ba_Hung/Exam/Spring_boot_web/api/src/main/java/com/dac/demo/controller/AdminController.java
@@ -5,6 +5,7 @@ import com.dac.demo.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,10 @@ import javax.validation.Valid;
 @RestController
 public class AdminController {
 
-    @Autowired(required = false)
+    @Autowired
     AdminService adminService;
 
+    @CrossOrigin(origins = "http://localhost:3001")
     @PostMapping("/create")
     public ResponseEntity<ServiceResult> createEmployee(@Valid @RequestBody AdminCreateUserRequest request) {
         return new ResponseEntity<>(adminService.createEmployee(request.getFirstName(),
