4a6213bffb3e0ff2fa552efaba6c6a8bfee051ef
huynhlv <huynhlevan1997@gmail.com>
2019-05-24 22:56:32 +0700

    check validate model user

--- Project/Ruby/ecommerce/app/models/user.rb
+++ Project/Ruby/ecommerce/app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
 
   enum role: {shop: 0, admin: 1}
 
-  validates :full_name, presence: true, length: {maximum: 50}
-  validates :phone_number, presence: true, length: {maximum: 11}
+  validates :full_name, presence: true, length: {maximum: 50, minimum: 6}
+  validates :phone_number, presence: true, length: {maximum: 11, minimum: 9}
 end
