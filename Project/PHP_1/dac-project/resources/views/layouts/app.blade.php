34e55f5b7d977c3729819e5b9d95d971dafa95f1
Thong <baibuvietnam@gmail.com>
2019-05-30 11:45:30 +0700

    validate client form register and login

--- Project/PHP_1/dac-project/resources/views/layouts/app.blade.php
+++ Project/PHP_1/dac-project/resources/views/layouts/app.blade.php
@@ -9,9 +9,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -19,7 +16,6 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
 </head>
 <body>
@@ -76,246 +72,142 @@
             </div>
         </div>
     </nav>
-    <div class="modal fade" id="modalForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-         aria-hidden="true">
-        <div class="modal-dialog cascading-modal" role="document">
-            <div class="modal-content">
-                <div class="modal-c-tabs">
-                    <ul class="justify-content-center nav nav-tabs md-tabs tabs-2 light-blue darken-3" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link active" data-toggle="tab" href="#panelLogin" role="tab">
-                                <i class="fas fa-user-plus mr-1"></i>
-                                Log In</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#panelRegister" role="tab"><i
-                                        class="fas fa-user mr-1"></i>
-                                Sign Up</a>
-                        </li>
-                    </ul>
-
-                    <div class="tab-content">
-                        <div class="tab-pane fade" id="panelRegister" role="tabpanel">
-                            <div class="card">
-                                <article class="card-body">
-                                    <form method="POST" id="formRegister">
-                                        @csrf
-                                        <div class="form-group">
-                                            <h2 class="heading-title">Sign Up For Free</h2>
-                                        </div>
-                                        <div class="form-row">
-                                            <div class="col form-group">
-                                                <!-- <label>First name </label> -->
-                                                <input id="first_name" type="text"
-                                                       class="form-control"
-                                                       name="first_name"
-                                                       value="{{ old('first_name') }}" required
-                                                       autocomplete="first_name" autofocus placeholder="First Name">
-
-                                                <span class="text-danger">
-                                                    <strong id="error-firstname"></strong>
-                                                </span>
-                                            </div>
-                                            <div class="col form-group">
-                                                <!-- <label>Last name</label> -->
-                                                <input id="last_name" type="text"
-                                                       class="form-control"
-                                                       name="last_name"
-                                                       value="{{ old('last_name') }}" required autocomplete="last_name"
-                                                       autofocus placeholder="Last Name">
-
-
-                                                <span class="text-danger">
-                                                    <strong id="error-lastname"></strong>
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <!-- <label>Email address</label> -->
-                                            <input id="email" type="email"
-                                                   class="form-control"
-                                                   name="email"
-                                                   value="{{ old('email') }}" required autocomplete="email"
-                                                   placeholder="Email Address">
-
-                                            <span class="text-danger">
-                                                <strong id="error-email"></strong>
-                                            </span>
-                                        </div>
-                                        <div class="form-group">
-                                            <!-- <label>Set a password</label> -->
-                                            <input id="password" type="password"
-                                                   class="form-control"
-                                                   name="password"
-                                                   required autocomplete="new-password" placeholder="Set a password">
-
-                                            <span class="text-danger">
-                                                <strong id="error-password"></strong>
-                                            </span>
-                                        </div>
-                                        <div class="form-group">
-                                            <!-- <label>Confirm password</label> -->
-
-                                            <input id="password-confirm" type="password" class="form-control"
-                                                   name="password_confirmation" required autocomplete="new-password"
-                                                   placeholder="Confirm Password">
-                                        </div>
-                                        <div class="form-group">
-                                            <!-- <label>Phone</label> -->
-                                            <input id="phone" type="text"
-                                                   class="form-control"
-                                                   name="phone"
-                                                   required value="{{ old('phone') }}"
-                                                   autocomplete="phone" placeholder="Phone Number">
-                                            <span class="text-danger">
-                                                <strong id="error-phone"></strong>
-                                            </span>
-                                        </div>
-                                        <div class="form-group">
-                                            <button type="submit" class="btn btn-primary btn-block custom-btn"
-                                                    id="submitFormSignUp">GET STARTED
-                                            </button>
-                                        </div>
-
-                                    </form>
-                                </article>
-                            </div>
-                        </div>
-                        <div class="tab-pane fade in show active" id="panelLogin" role="tabpanel">
-                            <div class="card">
-                                <article class="card-body">
-                                    <form method="POST" id="formLogin">
-                                        @csrf
-                                        <div class="form-group">
-                                            <!-- <label>Email address</label> -->
-                                            <!-- <span class="second-letter">*</span> -->
-                                            <input id="emailLogin" type="email"
-                                                   class="form-control"
-                                                   name="email"
-                                                   value="{{ old('email') }}" required autocomplete="email"
-                                                   placeholder="Email">
-
-                                            <span class="text-danger">
-                                                <strong id="error-emailLogin"></strong>
-                                            </span>
-                                        </div>
-                                        <div class="form-group">
-                                            <!-- <label>Password</label> -->
-                                            <!-- <span class="second-letter">*</span> -->
-                                            <input id="passwordLogin" type="password"
-                                                   class="form-control"
-                                                   name="password"
-                                                   required autocomplete="new-password" placeholder="Password">
-
-                                            <span class="text-danger">
-                                                <strong id="error-passwordLogin"></strong>
-                                            </span>
-                                        </div>
-                                        <span class="text-success">
-                                            <strong id="activate"></strong>
-                                        </span>
-                                        <div class="form-group">
-                                            <div class="col-md-6 offset-md-4">
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" name="remember"
-                                                           id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                                    <label class="form-check-label" for="remember">
-                                                        {{ __('Remember Me') }}
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <button type="submit" class="btn btn-primary btn-block custom-btn">LOGIN
-                                            </button>
-                                        </div>
-                                    </form>
-                                </article>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @component('components.login-register')
+    @endcomponent
     <main class="py-4">
         @yield('content')
     </main>
 </div>
+
+<!-- Scripts -->
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+
 </body>
 </html>
 <script type="text/javascript">
     $(document).ready(function () {
+        $("#formRegister").validate({
+            rules: {
+                first_name: {
+                    required: true,
+                },
+                last_name: {
+                    required: true,
+                },
+                email: {
+                    required: true,
+                    email: true,
+                },
+                password: {
+                    required: true,
+                    minlength: 8
+                },
+                password_confirmation: {
+                    required: true,
+                    equalTo: "#password"
+                },
+                phone: {
+                    required: true,
+                    number: true
+                }
+            },
+        });
+
+        $("#formLogin").validate({
+            rules: {
+                email: {
+                    required: true,
+                    email: true,
+                },
+                password: {
+                    required: true,
+                },
+            },
+        });
+
         $('#formRegister').submit(function (e) {
             e.preventDefault()
-            $('#error-firstname').html('')
-            $('#error-lastname').html('')
-            $('#error-email').html('')
-            $('#error-password').html('')
-            $('#error-phone').html('')
-            formInputs = $('#formRegister').serialize();
-            $.ajax({
-                url: '/register',
-                type: 'POST',
-                data: formInputs,
-                success: function (data) {
-                    $('#modalForm').modal("hide")
-                    Swal.fire({
-                        title: 'Successful',
-                        text: data.message,
-                        type: 'success',
-                    })
-                },
-                error: function (data) {
-                    errors = data.responseJSON.errors
-                    if (errors.first_name) {
-                        $('#error-firstname').html(errors.first_name[0]);
+            if ($('#formRegister').valid()) {
+                console.log('ok register');
+                $('#error-firstname').html('')
+                $('#error-lastname').html('')
+                $('#error-email').html('')
+                $('#error-password').html('')
+                $('#error-phone').html('')
+                formInputs = $('#formRegister').serialize();
+                $.ajax({
+                    url: '/register',
+                    type: 'POST',
+                    data: formInputs,
+                    success: function (data) {
+                        $('#modalForm').modal("hide")
+                        Swal.fire({
+                            title: 'Successful',
+                            text: data.message,
+                            type: 'success',
+                        })
+                    },
+                    error: function (data) {
+                        errors = data.responseJSON.errors
+                        if (errors.first_name) {
+                            $('#error-firstname').html(errors.first_name[0]);
+                        }
+                        if (errors.last_name) {
+                            $('#error-lastname').html(errors.last_name[0]);
+                        }
+                        if (errors.email) {
+                            $('#error-email').html(errors.email[0]);
+                        }
+                        if (errors.password) {
+                            $('#error-password').html(errors.password[0]);
+                        }
+                        if (errors.phone) {
+                            $('#error-phone').html(errors.phone[0]);
+                        }
                     }
-                    if (errors.last_name) {
-                        $('#error-lastname').html(errors.last_name[0]);
-                    }
-                    if (errors.email) {
-                        $('#error-email').html(errors.email[0]);
-                    }
-                    if (errors.password) {
-                        $('#error-password').html(errors.password[0]);
-                    }
-                    if (errors.phone) {
-                        $('#error-phone').html(errors.phone[0]);
-                    }
-                }
-            });
+                });
+            } else {
+                console.log('error register');
+            }
+
+
         });
 
         $('#formLogin').submit(function (e) {
             e.preventDefault()
-            $('#error-emailLogin').html('')
-            $('#error-passwordLogin').html('')
-            $('#activate').html('')
-            formInputs = $('#formLogin').serialize();
-            $.ajax({
-                url: '/login',
-                type: 'POST',
-                data: formInputs,
-                success: function (data) {
-                    if (data.message) {
-                        $('#activate').html(data.message)
-                    } else {
-                        $('#modalForm').modal("hide")
-                        location.reload()
+            if ($('#formLogin').valid()) {
+                console.log('ok Login')
+                $('#error-emailLogin').html('')
+                $('#error-passwordLogin').html('')
+                $('#activate').html('')
+                formInputs = $('#formLogin').serialize();
+                $.ajax({
+                    url: '/login',
+                    type: 'POST',
+                    data: formInputs,
+                    success: function (data) {
+                        if (data.message) {
+                            $('#activate').html(data.message)
+                        } else {
+                            $('#modalForm').modal("hide")
+                            location.reload()
+                        }
+                    },
+                    error: function (data) {
+                        errors = data.responseJSON.errors
+                        if (errors.email) {
+                            $('#error-emailLogin').html(errors.email[0]);
+                        }
+                        if (errors.password) {
+                            $('#error-passwordLogin').html(errors.password[0]);
+                        }
                     }
-                },
-                error: function (data) {
-                    errors = data.responseJSON.errors
-                    if (errors.email) {
-                        $('#error-emailLogin').html(errors.email[0]);
-                    }
-                    if (errors.password) {
-                        $('#error-passwordLogin').html(errors.password[0]);
-                    }
-                }
-            });
+                });
+            }else{
+                console.log('error Login')
+            }
         });
     })
 </script>
--- Project/PHP_1/dac-project/resources/views/test.blade.php
+++ Project/PHP_1/dac-project/resources/views/test.blade.php
@@ -1,1 +1,1 @@
-test abcxyz
+abcxyz
