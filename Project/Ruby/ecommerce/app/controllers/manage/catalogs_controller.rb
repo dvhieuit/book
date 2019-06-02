2f71754aaef041c493fcdc7ddc6878c34ee3d3dd
huynhlv <huynhlevan1997@gmail.com>
2019-06-02 11:29:14 +0700

    modal catalogs new edit delete

--- Project/Ruby/ecommerce/app/controllers/manage/catalogs_controller.rb
+++ Project/Ruby/ecommerce/app/controllers/manage/catalogs_controller.rb
@@ -1,13 +1,9 @@
 class Manage::CatalogsController < ApplicationController
+  before_action :load_catalogs, except: %i(create new index)
+
   def create
     @catalog = Catalog.new(catalog_params)
-    respond_to do |format|
-      if @catalog.save
-        format.js
-      else
-        format.json { render json: @catalog.errors, status: :unprocessable_entity }
-      end
-    end
+    @catalog.save
   end
 
   def index
@@ -15,9 +11,27 @@ class Manage::CatalogsController < ApplicationController
     @catalogs = Catalog.order_by_create.paginate(page: params[:page], per_page: 25)
   end
 
+  def edit
+  end
+
+  def update
+    @catalog.update_attributes(catalog_params)
+  end
+
+  def destroy
+    @catalog.destroy
+  end
+
   private
 
   def catalog_params
     params.require(:catalog).permit(:name)
+  end
+
+  def load_catalogs
+    @catalog = Catalog.find_by(id: params[:id])
+    return if @catalog
+    flash[:danger] = "Catalog not found"
+    redirect_to manage_catalogs_path
   end
 end
